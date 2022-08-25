ba0edd5a2a99a8f1178a9b2657fc0c36d7826cf2
Indera Ihsan <inderaihsan@yahoo.com>
2022-08-25 13:48:49 +0700

    init

--- first web server/app.js
+++ first web server/app.js
@@ -24,6 +24,15 @@ app.get('/about', (req,res)=>{
 }) 
 
 
+app.get('/about/:name', (req,res)=>{
+     const hero = herofunc.find_hero(req.params.name)   
+     const hero_type = hero.type 
+     const hero_name = hero.name.toUpperCase()
+     title = 'About ' + hero_name
+
+     res.render('detail', {hero_name, hero_type, layout : 'layout/main layout', title})
+})
+
 app.use('/', (req,res)=>{ 
      const char_name = herofunc.duelist.name  
      const char_zeni = herofunc.duelist.zeni
--- first web server/models/hero_owner.js
+++ first web server/models/hero_owner.js
@@ -4,13 +4,15 @@ const folderdataexist = fs.existsSync('./data')
 
 const check_dat = () =>{
     if (!folderdataexist){ 
-        fs.mkdir('data',(err)=>{
+        fs.mkdir('./data',(err)=>{
             if(err) throw err
         })
     }     
     if (! fs.existsSync('./data/hero.json')){
         fs.writeFileSync('./data/hero.json', '[]','utf8')}
-}  
+}   
+
+fs.writeFileSync('./data/hero.json', '[]','utf8')
 
 check_dat()
 
@@ -51,14 +53,22 @@ class owner{
             skill : skill, 
             price : price, 
         }  
-        this.owned_hero.push(build) 
-        this.zeni = this.zeni - price
         const fetched = fs.readFileSync('./data/hero.json', 'utf8') 
-        const jsoned = JSON.parse(fetched) 
-        jsoned.push(build) 
-        fs.writeFileSync('./data/hero.json', JSON.stringify(jsoned), 'utf8')
-        
-        
+        let jsoned = JSON.parse(fetched)  
+        let there=false 
+        let items =0
+        for (let items in jsoned) {
+            if (name == jsoned[items].name){
+                there=true 
+                console.log('hero already written in json') 
+                items =items++
+            }
+        } 
+        if (!there){
+            jsoned.push(build) 
+            this.owned_hero.push(build) 
+            this.zeni = this.zeni - price }
+        fs.writeFileSync('./data/hero.json', JSON.stringify(jsoned), 'utf8') 
     }
 } 
 
@@ -66,17 +76,42 @@ const load_hero = () =>{
     const fetched_file = fs.readFileSync('./data/hero.json', 'utf8') 
     const allhero = JSON.parse(fetched_file) 
     return allhero
-}
+} 
+
+const find_hero = (name)=>{
+    all_hero = JSON.parse(fs.readFileSync('./data/hero.json', 'utf8')) 
+    let nam = 0
+    for (nam in all_hero){
+        if (name == all_hero[nam].name){
+            console.log(all_hero[nam]) 
+            console.log('found!', nam) 
+            return all_hero[nam] 
+
+            
+        } 
+        nam = nam+1
+        
+    }
+   
+
+} 
 
 duelist = new owner('arlene', 'agile', 9500)   
 duelist.ownhero('link', 'necromancer', 'lifesteal',2000) 
-//duelist.ownhero('hyun', 'assassin', 'stealth',2000) 
+duelist.ownhero('hyun', 'assassin', 'stealth',2000)  
 
-console.log(load_hero)
+
+
+
+
+console.log(find_hero('hyun')) 
+
+
 
 
  module.exports = {
      duelist, 
-     load_hero 
+     load_hero, 
+     find_hero
 } 
 
